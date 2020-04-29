6744f0925ef225d505c1025e4e239f831bc81b36
sungg <zwsun1948@163.com>
2020-04-29 23:58:27 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,68 @@
+const $siteList = $(".siteList");
+const $lastLi = $siteList.find("li.last");
+const x = localStorage.getItem("x");
+const xObject = JSON.parse(x);
+const hashMap = xObject || [
+  { logo: "A", url: "https://www.acfun.cn" },
+  { logo: "B", url: "https://www.bilibili.com" },
+];
+const simplifyUrl = (url) => {
+  return url
+    .replace("http://", "")
+    .replace("https://", "")
+    .replace("www.", "")
+    .replace(/\/.*/, "");
+};
+const render = () => {
+  $siteList.find("li:not(.last)").remove();
+  hashMap.forEach((node,index) => {
+    const $li = $(`<li>
+      <div class="site">
+          <div class="logo">${simplifyUrl(
+            node.url
+          )[0].toUpperCase()}</div>
+          <div class="link">${simplifyUrl(node.url)}</div>
+          <div class="close">
+            <svg class="icon" aria-hidden="true">
+              <use xlink:href="#icon-close"></use>
+            </svg>
+          </div>
+      </div> 
+    </li>`).insertBefore($lastLi);
+    $li.on('click',()=>{
+      window.open(node.url)
+    })
+    $li.on('click','.close',(e)=>{
+      e.stopPropagation()
+      hashMap.splice(index,1)
+      render()
+    })
+  });
+};
+render();
+
+$(".addButton").on("click", () => {
+  let url = window.prompt("请问你要添加的网址是啥？");
+  if (url.indexOf("http") !== 0) {
+    url = "https://" + url;
+  }
+  hashMap.push({
+    logo: simplifyUrl(url)[0].toUpperCase(),
+    url: url,
+  });
+  render();
+});
+
+window.onbeforeunload = () => {
+  const string = JSON.stringify(hashMap);
+  localStorage.setItem("x", string);
+};
+
+$(document).on('keypress',(e)=>{
+  const {key} = e
+  hashMap.forEach.call(hashMap,(node)=>{
+    if(node.logo.toLowerCase() === key){
+      window.open(node.url)
+    }
+  })
+})
